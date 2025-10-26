713e95fd43007de9b565085da56875ed9cff3859
Bishflix <bishflix2@gmail.com>
2025-10-26 20:55:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7263388532-ed36d",
+  "appId": "1:102429475951:web:a990cd81299e54d407799a",
+  "apiKey": "AIzaSyCC0BFOhq9DW4xvK6Gkod8lZmuvmrkaYFM",
+  "authDomain": "studio-7263388532-ed36d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "102429475951"
+};
